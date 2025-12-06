eb925d4bbb77f36b121cea4d67887a22246333c2
Younes Tsouli <younestsouli444@gmail.com>
2025-12-06 22:36:16 +0100

    feat: creating the grids for the game #1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Happy developing ✨')
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
+const b5 = document.getElementById("b5");
+const b6 = document.getElementById("b6");
+const b7 = document.getElementById("b7");
+const b8 = document.getElementById("b8");
+const b9 = document.getElementById("b9");
+
